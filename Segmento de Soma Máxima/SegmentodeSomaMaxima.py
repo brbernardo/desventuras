74a225c5da563f8171f7a79f22e67ccea8ac7de7
Luiz Bernardo <eu@bernardo.net.br>
2020-09-15 15:59:32 -0300

    segmento

--- Segmento de Soma Máxima/SegmentodeSomaMaxima.py
+++ Segmento de Soma Máxima/SegmentodeSomaMaxima.py
@@ -20,14 +20,14 @@ def SegmentoSomaMaxima(A, low, mid, high):
             maxRight = i
     return maxLeft, maxRight, (leftSum + rightSum)
 
-def findMaxSubarray(A, low, high):
+def Subarray(A, low, high):
     if high == low:
         return low, high, A[low]
     else:
         mid = int((low + high) / 2)
 
-        leftLow, leftHigh, leftSum = findMaxSubarray(A, low, mid)
-        rightLow, rightHigh, rightSum = findMaxSubarray(A, mid + 1, high)
+        leftLow, leftHigh, leftSum = Subarray(A, low, mid)
+        rightLow, rightHigh, rightSum = Subarray(A, mid + 1, high)
         crossLow, crossHigh, crossSum = SegmentoSomaMaxima(A, low, mid, high)
         
         if((leftSum >= rightSum) and (leftSum >= crossSum)):
@@ -39,4 +39,4 @@ def findMaxSubarray(A, low, high):
         
 if __name__== "__main__":
     A = [-16, 20, -10, 12, 27, -6, -4, 8]
-    print(findMaxSubarray(A, 0, len(A) - 1))
+    print(Subarray(A, 0, len(A) - 1))
